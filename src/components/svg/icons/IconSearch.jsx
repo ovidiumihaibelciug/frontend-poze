65d1d5c93052c04d85d833a3265ffc1c261fb71e
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-05-07 15:22:19 +0300

    stuff

---
+++ src/components/svg/icons/IconSearch.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const IconSearch = props => (
+    <svg
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        viewBox="0 0 24 24"
+        version={1.1}
+        width="1em"
+        height="1em"
+        {...props}
+    >
+        <circle cx={11} cy={11} r={8} />
+        <path d="M21 21l-4.35-4.35" />
+    </svg>
+);
+
+export default IconSearch;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import classNames from 'classnames';
+import Logo from "../components/svg/imgs/Logo";
+import {Link, NavLink} from 'react-router-dom';
+import IconSearch from "../components/svg/icons/IconSearch";
 
 class Home extends Component {
 
@@ -10,25 +13,86 @@ class Home extends Component {
   };
 
   componentDidMount() {
-    const { time } = this.state;
+    const {time} = this.state;
     setTimeout(() => {
       this.setState({
         second: true
       });
+      setTimeout(() => {
+        this.setState({
+          anim: false
+        })
+      }, 500)
     }, time);
   }
 
   render() {
-    const { anim, second, time } = this.state;
-    
+    const {anim, second, time} = this.state;
+
     return (
-      <div className="home">
-        <div className={classNames({ 'home__anim': anim, 'home__anim--second': second })}>
-          {
-            <div className="home__anim__line" style={{ animationDuration: time + 'ms' }} />
+      <Fragment>
+        <div className="home">
+          {anim ? (
+            <div className={classNames({'home__anim': anim, 'home__anim--second': second})}>
+              <div className="home__anim__line" style={{animationDuration: time + 'ms'}}/>
+            </div>
+          ) : <Fragment>
+            <div className="navbar">
+              <div className="navbar__logo__container">
+                <Logo className="navbar__logo"/>
+              </div>
+              <div className="navbar__links">
+                <NavLink to={'/'} className="navbar__link" activeClassName="navbar__link--active">Home</NavLink>
+                <NavLink to={'/categories'} className="navbar__link"
+                         activeClassName="navbar__link--active">Categories</NavLink>
+                <NavLink to={'/categories'} className="navbar__link"
+                         activeClassName="navbar__link--active">Test</NavLink>
+                <NavLink to={'/categories'} className="navbar__link"
+                         activeClassName="navbar__link--active">Stuff</NavLink>
+              </div>
+            </div>
+            <div className="home__inner">
+              <div className="home__details">
+                <div className="home__details__title">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas!
+                </div>
+                <div className="home__details__description">
+                  Lorem ipsum dolor sit amet.
+                </div>
+              </div>
+              <div className="home__pics">
+                <div className="home__pics__row">
+                  <div className="home__pics__item asd"></div>
+                  <div className="home__pics__item asd2"></div>
+                </div>
+                <div className="home__pics__row">
+                  <div className="home__pics__item asd3"></div>
+                  <div className="home__pics__item asd4"></div>
+
+                </div>
+              </div>
+            </div>
+            <div className="gallery">
+              <div className="gallery__inner">
+                <div className="gallery__input__container">
+                  <input className="gallery__input" type="text" placeholder="asd" />
+                  <IconSearch className="gallery__input__icon" />
+                </div>
+                <div className="gallery__images">
+                  dadasdad
+                  asdas
+                  das
+                  d
+                  asd
+                </div>
+              </div>
+            </div>
+          </Fragment>
+
           }
         </div>
-      </div>
+
+      </Fragment>
     );
   }
 }
